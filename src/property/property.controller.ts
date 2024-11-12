71f06d2c37d4238c889058053fb9a0799331debf
RohitSth <shrestha.rohit655@gmail.com>
2024-11-12 11:53:45 +0545

    learn :: learned how to create routes for get and post

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put } from '@nestjs/common';
 
 @Controller('property')
-export class PropertyController {}
+export class PropertyController {
+  @Get()
+  findAll() {
+    return 'This action returns all properties';
+  }
+
+  @Post()
+  create() {
+    return 'This action adds a new property';
+  }
+
+  @Put()
+  update() {
+    return 'This action updates a property';
+  }
+}
