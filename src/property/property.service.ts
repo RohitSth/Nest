7a5407cde6843920e2989a21870370a06444214b
RohitSth <shrestha.rohit655@gmail.com>
2024-11-13 13:54:23 +0545

    update :: learned how to do get all

--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Property } from 'src/entities/property.entity';
 import { Repository } from 'typeorm';
@@ -10,13 +10,19 @@ export class PropertyService {
     @InjectRepository(Property)
     private propertyRepository: Repository<Property>,
   ) {}
-  async findAll() {}
+  async findAll() {
+    const properties = await this.propertyRepository.find();
+    if (!properties) throw new NotFoundException();
+    return properties;
+  }
   async findOne(id: number) {
-    return await this.propertyRepository.findOne({
+    const property = await this.propertyRepository.findOne({
       where: {
         id,
       },
     });
+    if (!property) throw new NotFoundException();
+    return property;
   }
   async create(dto: CreatePropertyDto) {
     return await this.propertyRepository.save(dto);
